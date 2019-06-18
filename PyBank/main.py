8a06e4032bdb81821bbbf67697a85c8c5c1e9ab8
Tolly Smith <tolly229@yahoo.com.tw>
2019-06-18 06:28:07 -0500

    update 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,6 +17,7 @@ with open(path, 'r') as csvfile:
     for row in csvreader: 
         month.append(row[0])
         profit.append(int(row[1]))
+        
         profit_losses.append(int(row[1])-int(previous_value))
 
         previous_value = row[1]
@@ -29,12 +30,12 @@ with open(path, 'r') as csvfile:
     print(f"Total: $ {sum(profit)}")
     print(f"Average Change: $ {(averageChange)}")
     print(profit_losses)
+
     
     
     
     
-    
-    #print("Greatest Increase in Profits: "  {str(greatIncreaseMonth) + {str(greatIncreaseAmount)})
+    #print(f"Greatest Increase in Profits:  {str(greatIncreaseMonth) + {str(greatIncreaseAmount)})
     #print( \n"Greatest Decrease in Profits: " + {str(greatDecreaseMonth)} + {str(greatDecreaseAmount)})
 
 
